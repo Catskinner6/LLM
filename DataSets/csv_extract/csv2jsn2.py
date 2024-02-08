92c6cddd5c69a43224f0beba34c0e639d98d9521
Scott <ser.arroz@gmail.com>
2024-02-08 14:43:43 -0700

    train_short.csv created.

---
+++ DataSets/csv_extract/csv2jsn2.py
@@ -0,0 +1,32 @@
+# Convert a CSV table to a JSON table in a new file
+
+import csv
+import json
+
+
+csv_file_path = 'r1_short.csv'  # Replace with the path to your CSV file
+selected_column1 = 'text'  # Replace with the name of the column you want to keep
+selected_column2 = 'label' # Replace with the name of the column you want to keep
+output_json_file = 'train_short.json'  # Replace with the desired output JSON file path
+
+csvfile = open(csv_file_path, 'r', newline='', encoding='utf-8')  # Specify encoding if needed
+jsonfile = open(output_json_file, 'w')
+
+# Use DictReader without specifying fieldnames
+reader = csv.DictReader(csvfile)
+
+for row in filter(lambda row: row[selected_column1].strip(), reader):
+    # Create a dictionary with only the selected columns
+    selected_data = {
+        'text': row[selected_column1],
+        'label': row[selected_column2]
+    }
+    
+    # Write the selected data to the JSON file
+    json.dump(selected_data, jsonfile)
+    jsonfile.write('\n')
+
+csvfile.close()
+jsonfile.close()
+
+print(f'The result has been saved to {output_json_file}')
